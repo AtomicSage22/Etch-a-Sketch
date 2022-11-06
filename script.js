4498922e182ca41f6255760b4beca7594e57e405
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2022-11-06 09:48:48 +0530

    add mouse hover trail

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let container = document.querySelector(".container")
-let dimensions = 8;
-for (i = 0; i < (dimensions*dimensions) ; i++){
+const container = document.querySelector(".container")
+let dimensions = 20;
+for (i = 0; i < (dimensions*dimensions) ; i++)
     {
         const box = document.createElement("div");
         box.classList.add("box");
@@ -8,4 +8,7 @@ for (i = 0; i < (dimensions*dimensions) ; i++){
         box.style.height = `${100/dimensions}%`;
         container.append(box);
     }
-}
+const box = document.querySelectorAll(".box");
+box.forEach((e) => {
+    e.addEventListener("mouseover", () => e.style.backgroundColor = "red");
+});
