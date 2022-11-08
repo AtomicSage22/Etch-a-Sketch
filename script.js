df408861eaee65071301d1d449baa4d79dadc8e1
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2022-11-08 09:07:47 +0530

    Add working grid customization

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const container = document.querySelector(".gridContainer");
-let dimensions = 20;
-const button = document.querySelector(".gridSize");
-button.addEventListener("click", () => {dimensions = prompt("Enter Grid Size")});
+const container = document.querySelector(".gridContainer");         
+let dimensions = 16;                                            //Initial grid dimensions
+const button = document.querySelector(".gridSize");             
 
-for (i = 0; i < (dimensions*dimensions) ; i++)
+const generateGrid = (dimensions) => {                          //Function to generate grid 
+    for (i = 0; i < (dimensions*dimensions) ; i++)              //based on given dimensions
     {
         const box = document.createElement("div");
         box.classList.add("box");
@@ -11,10 +11,25 @@ for (i = 0; i < (dimensions*dimensions) ; i++)
         box.style.height = `${100/dimensions}%`;
         container.append(box);
     }
+    const box = document.querySelectorAll(".box");
+    box.forEach((e) => {                                        //Function to add hover effect
+    e.addEventListener("mouseover", () => e.style.backgroundColor = "Black");});
+};
 
-const box = document.querySelectorAll(".box");
-box.forEach((e) => {
-    e.addEventListener("mouseover", () => e.style.backgroundColor = "white");
+generateGrid(dimensions);
+
+
+button.addEventListener("click", () => {                        //Function to change grid 
+    dimensions = prompt("Enter Grid Size");
+    if(dimensions<100){
+        while(container.firstChild){                            //dimensions and generate new, working grid
+            container.removeChild(container.firstChild);
+        }
+        generateGrid(dimensions);
+    }
+    else{
+        alert("Enter Dimensions should be less than 100.");
+    }
 });
 
 
